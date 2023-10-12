34a5da6db28b153090679502176db6cd31df7b48
rizzzse <rizzzse@gmail.com>
2023-10-12 20:48:07 +0900

    add iced app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    widget::{container, text},
+    Sandbox, Settings,
+};
+
+struct App {}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl Sandbox for App {
+    type Message = Message;
+
+    fn new() -> Self {
+        App {}
+    }
+    fn title(&self) -> String {
+        String::from("Browser switch")
+    }
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        container(text("Browser switch")).into()
+    }
+}
+
+fn main() -> iced::Result {
+    App::run(Settings::default())
 }
